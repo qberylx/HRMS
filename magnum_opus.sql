463cdf7ed33255b17b4129f51c739feb7565f97e
takerukimura <tk.sama87@gmail.com>
2021-10-13 11:06:15 +0800

    database for this system

---
+++ magnum_opus.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 13, 2021 at 05:05 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `magnum_opus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cm01_mohon`
+--
+
+CREATE TABLE `cm01_mohon` (
+  `cm01_id` bigint(20) NOT NULL,
+  `cm01_userid` bigint(20) NOT NULL,
+  `cm01_sysid` bigint(20) NOT NULL,
+  `cm01_modulid` bigint(20) NOT NULL,
+  `cm01_klasimodul` tinyint(1) NOT NULL DEFAULT 0,
+  `cm01_klasiproses` tinyint(1) NOT NULL DEFAULT 0,
+  `cm01_klasiskrin` tinyint(1) NOT NULL DEFAULT 0,
+  `cm01_klasibug` tinyint(1) NOT NULL DEFAULT 0,
+  `cm01_klasilaporan` tinyint(1) NOT NULL DEFAULT 0,
+  `cm01_ulasan` text NOT NULL,
+  `cm01_datecreated` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cm01_mohon`
+--
+
+INSERT INTO `cm01_mohon` (`cm01_id`, `cm01_userid`, `cm01_sysid`, `cm01_modulid`, `cm01_klasimodul`, `cm01_klasiproses`, `cm01_klasiskrin`, `cm01_klasibug`, `cm01_klasilaporan`, `cm01_ulasan`, `cm01_datecreated`) VALUES
+(1, 1, 1, 1, 0, 1, 1, 0, 0, 'xcxxcxcxcxcxc', '2021-10-11 02:10:20'),
+(2, 1, 2, 2, 1, 0, 0, 0, 0, 'hahahahahaha', '2021-10-11 02:36:02'),
+(3, 1, 2, 2, 1, 0, 0, 0, 0, 'hahahahahaha', '2021-10-11 02:37:13'),
+(4, 1, 2, 2, 1, 0, 0, 0, 0, 'hahahahahaha', '2021-10-11 02:37:55'),
+(5, 1, 2, 2, 1, 0, 0, 0, 0, 'hahahahahaha', '2021-10-11 02:38:49'),
+(6, 1, 1, 7, 0, 1, 1, 0, 0, 'testing fully', '2021-10-11 07:23:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cm02_lampiran`
+--
+
+CREATE TABLE `cm02_lampiran` (
+  `cm02_id` bigint(20) NOT NULL,
+  `cm02_mohonid` bigint(20) NOT NULL,
+  `cm02_namadokumen` varchar(100) NOT NULL,
+  `cm02_lokasi` varchar(500) NOT NULL,
+  `cm02_datecreated` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cm02_lampiran`
+--
+
+INSERT INTO `cm02_lampiran` (`cm02_id`, `cm02_mohonid`, `cm02_namadokumen`, `cm02_lokasi`, `cm02_datecreated`) VALUES
+(1, 6, '1633937003_b6e714f1f2fbdc7614fe.png', '', '2021-10-11 07:23:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cm_modul`
+--
+
+CREATE TABLE `cm_modul` (
+  `id` bigint(20) NOT NULL,
+  `kod` varchar(5) NOT NULL,
+  `sistemid` bigint(20) NOT NULL,
+  `namamodul` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cm_modul`
+--
+
+INSERT INTO `cm_modul` (`id`, `kod`, `sistemid`, `namamodul`) VALUES
+(1, '01', 1, 'BELANJAWAN'),
+(2, '03', 1, 'PEROLEHAN'),
+(3, '04', 1, 'PEMBAYARAN'),
+(4, '05', 1, 'TERIMAAN'),
+(5, '06', 1, 'PENDAHULUAN'),
+(6, '07', 1, 'PANJAR WANG RUNCIT'),
+(7, '08', 1, 'INVOIS'),
+(8, '22', 1, 'NOTA KREDIT NOTA DEBIT (PEMBAYARAN)'),
+(9, '10', 1, 'PENGURUSAN INVENTORI'),
+(10, '11', 1, 'PENYATA PENYESUAIAN BANK'),
+(11, '12', 1, 'NOTA KREDIT NOTA DEBIT'),
+(12, '13', 1, 'SIMPANAN TETAP'),
+(13, '14', 1, 'JURNAL'),
+(14, '23', 1, 'HAPUS KIRA'),
+(15, '16', 1, 'PENUTUPAN AKAUN'),
+(16, '17', 1, 'UTILITI'),
+(17, '18', 1, 'PENTADBIR SISTEM'),
+(18, '02', 1, 'SYARIKAT'),
+(19, '19', 1, 'PEROLEHAN (TEST MENU)'),
+(20, '20', 1, 'LAPORAN KEWANGAN'),
+(21, '21', 1, 'VERIFIKASI DATA'),
+(22, '99', 2, 'PENTADBIR SISTEM'),
+(23, '01', 2, 'PERIBADI'),
+(24, '02', 2, 'ROC'),
+(25, '03', 2, 'CUTI'),
+(26, '04', 2, 'LATIHAN'),
+(27, '05', 2, 'PERAKAM WAKTU'),
+(28, '06', 2, 'GAJI'),
+(29, '07', 2, 'TUNTUTAN'),
+(30, '08', 2, 'PENILAIAN PRESTASI'),
+(31, '09', 2, 'UTILITI'),
+(32, '10', 2, 'SKT'),
+(33, '11', 2, 'PERMIT MENGAJAR'),
+(34, '12', 2, 'LAPORAN'),
+(35, '13', 2, 'SLIP GAJI'),
+(36, '14', 2, 'PENYATA PENDAPATAN'),
+(37, '15', 2, 'KAJI SELIDIK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cm_sistem`
+--
+
+CREATE TABLE `cm_sistem` (
+  `id` bigint(20) NOT NULL,
+  `kod` varchar(5) NOT NULL,
+  `namasistem` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cm_sistem`
+--
+
+INSERT INTO `cm_sistem` (`id`, `kod`, `namasistem`) VALUES
+(1, 'SPK', 'SISTEM PENGURUSAN KEWANGAN'),
+(2, 'SPSM', 'SISTEM PENGURUSAN SUMBER MANUSIA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ut_menu`
+--
+
+CREATE TABLE `ut_menu` (
+  `id` bigint(20) NOT NULL,
+  `nama_menu` varchar(50) NOT NULL,
+  `parent` bigint(20) NOT NULL,
+  `urutan` int(11) NOT NULL,
+  `menu_url` varchar(100) NOT NULL,
+  `menu_level` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ut_menu`
+--
+
+INSERT INTO `ut_menu` (`id`, `nama_menu`, `parent`, `urutan`, `menu_url`, `menu_level`) VALUES
+(1, 'Aduan', 0, 99, '/home', 0),
+(2, 'Utiliti', 0, 99, '/utilities', 0),
+(3, 'Menu', 2, 99, '/utilities/menu', 1),
+(4, 'Permohonan', 1, 99, '/home/index', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cm01_mohon`
+--
+ALTER TABLE `cm01_mohon`
+  ADD PRIMARY KEY (`cm01_id`);
+
+--
+-- Indexes for table `cm02_lampiran`
+--
+ALTER TABLE `cm02_lampiran`
+  ADD PRIMARY KEY (`cm02_id`),
+  ADD KEY `fk_cm02_mohonid_cm01_id` (`cm02_mohonid`);
+
+--
+-- Indexes for table `cm_modul`
+--
+ALTER TABLE `cm_modul`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cm_sistem`
+--
+ALTER TABLE `cm_sistem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ut_menu`
+--
+ALTER TABLE `ut_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cm01_mohon`
+--
+ALTER TABLE `cm01_mohon`
+  MODIFY `cm01_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `cm02_lampiran`
+--
+ALTER TABLE `cm02_lampiran`
+  MODIFY `cm02_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `cm_modul`
+--
+ALTER TABLE `cm_modul`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `cm_sistem`
+--
+ALTER TABLE `cm_sistem`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `ut_menu`
+--
+ALTER TABLE `ut_menu`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cm02_lampiran`
+--
+ALTER TABLE `cm02_lampiran`
+  ADD CONSTRAINT `fk_cm02_mohonid_cm01_id` FOREIGN KEY (`cm02_mohonid`) REFERENCES `cm01_mohon` (`cm01_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
