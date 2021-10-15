51dd34c08efb66500560302be2d969355fe92325
takerukimura <tk.sama87@gmail.com>
2021-10-15 10:11:51 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/magnum_opus';
+    public $baseURL = 'http://localhost/spa';
 
     /**
      * --------------------------------------------------------------------------
